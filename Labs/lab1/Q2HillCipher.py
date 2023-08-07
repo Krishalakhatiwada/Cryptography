26a3ea3f77beb1631ca5aebcc3677f20b74896ef
Krishala Khatiwada <krishalakhatiwoda1999@gmail.com>
2023-08-07 22:44:11 +0545

    Hill Cipher

--- Labs/lab1/Q2HillCipher.py
+++ Labs/lab1/Q2HillCipher.py
@@ -1,6 +1,58 @@
-print=input("Enter your name:").upper().replace(' ','')
+import numpy as np
 
-key=[[2, 4], [3,5]]
+# Functions
 
-alphabets={chr(65+i):i for i in range(26)}
-rev_alphabtes={value:key for key,value in alphabets.items()}
+def cipherDecipher(text, key):
+    alphabets = [ord(char) - ord('A') for char in text]
+    n = len(key)
+    num_of_padding = (len(alphabets) % n)
+    alphabets += [1] * num_of_padding
+
+    text=''
+    for i in range(0, len(alphabets), n):
+        block = alphabets[i : i + n]
+        product = np.dot(key, block) % 26
+        text += "".join(chr(65 + i) for i in product)
+
+    return text
+
+def inverseOfKey(key):
+    # Determinant
+    determinant = int(np.ceil(np.linalg.det(key) % 26))
+
+    #Finding 1/det 
+    for i in range(26):
+        if (determinant * i) % 26 == 1:
+            determinant = i
+            break
+
+    # Adjoint
+    adjKey = np.linalg.inv(key) * np.linalg.det(key)
+
+    for index in range(len(adjKey)):
+        for col_index in range(len(adjKey)):
+            if adjKey[index][col_index] < 0:
+                adjKey[index][col_index] = np.ceil(adjKey[index][col_index] % 26)
+
+    adjKey = adjKey.astype(int)
+
+    return ((determinant * adjKey) % 26)
+
+
+#Input Data
+
+plain_text = "Shradha"
+plain_text = plain_text.upper().replace(" ", "")
+
+key = np.array([[5, 8], [17, 3]])
+
+# Encryption
+cipher_text = cipherDecipher(plain_text, key)
+print("Cipher Text: " + cipher_text)
+
+#inverse of the key.
+inv_key = inverseOfKey(key)
+
+# Decryption
+decipher_text = cipherDecipher(cipher_text, inv_key)
+print("Deciphered Text: " + decipher_text[:(len(cipher_text)-1)]) #omiting the padded numbers
